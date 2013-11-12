bee1834405ee25d355754e581e7adc3190748301
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2013-11-11 17:31:19 -0800

    We cannot guarantee that grunt watch will do the same as grunt default

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
                 '<%= jshint.lib.src %>',
                 '<%= jshint.test.src %>'
             ],
-            tasks: ['default']
+            tasks: ['lint', 'test']
         }
     });
 
