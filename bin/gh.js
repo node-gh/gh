255b1b768e8efa3a5ac3b73f33006b640fece63c
Henrique Vicente <henriquevicente@gmail.com>
2015-03-06 09:52:28 -0300

    Moving start up sequence to lib/cmd.js from bin/gh.js.
    Renaming the cmd.run method to cmd.setUp.
    Creating a new cmd.run method with the moved start up sequence.

--- bin/gh.js
+++ bin/gh.js
@@ -11,43 +11,10 @@
 
 'use strict';
 
-var path = require('path'),
-    fs = require('fs'),
-    logger = require('../lib/logger'),
-    pkg = require('../package.json'),
-    semver = require('semver'),
-    tracker = require('../lib/tracker'),
-    configs = require('../lib/configs'),
-    verbose = process.argv.indexOf('--verbose') === true;
-
-// Check node version
-function isCompatibleNodeVersion() {
-    return semver.satisfies(process.version, pkg.engines.node);
-}
+var verbose = process.argv.indexOf('--verbose') === true;
 
 if (verbose) {
     process.env.GH_VERBOSE = true;
 }
 
-if (!isCompatibleNodeVersion()) {
-    logger.error('Please update your NodeJS version: http://nodejs.org/download');
-}
-
-if (!fs.existsSync(configs.getUserHomePath())) {
-    configs.createGlobalConfig();
-}
-
-// If configs.PLUGINS_PATH_KEY is undefined, try to cache it before proceeding.
-if (configs.getConfig()[configs.PLUGINS_PATH_KEY] === undefined) {
-    configs.getNodeModulesGlobalPath();
-}
-
-// -- Env ------------------------------------------------------------------------------------------
-try {
-    process.env.GH_PATH = path.join(__dirname, '../');
-
-    require('../lib/cmd.js').run();
-} catch (e) {
-    tracker.track('error');
-    console.error(e.stack || e);
-}
+require('../lib/cmd.js').run();
