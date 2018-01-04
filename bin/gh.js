c34736a2a4f30d76f698a2577b51f508fcb56297
protoEvangelion <ryantgarant@gmail.com>
2018-01-03 22:17:09 -0800

    Source Formatting

--- bin/gh.js
+++ bin/gh.js
@@ -1,25 +1,28 @@
 #!/usr/bin/env node
 
 /*
- * Copyright 2013-2015, All Rights Reserved.
+ * Copyright 2013-2018, All Rights Reserved.
  *
  * Code licensed under the BSD License:
  * https://github.com/node-gh/gh/blob/master/LICENSE.md
  *
- * @author Eduardo Lundgren <edu@rdo.io>
+ * @author Henrique Vicente <henriquevicente@gmail.com>
+ * @author Eduardo Lundgren <eduardo.lundgren@gmail.com>
+ * @author Zeno Rocha <zno.rocha@gmail.com>
+ * @author Ryan Garant <ryantgarant@gmail.com>
  */
 
-'use strict';
+'use strict'
 
-var verbose = process.argv.indexOf('--verbose') !== -1;
-var insane = process.argv.indexOf('--insane') !== -1;
+var verbose = process.argv.indexOf('--verbose') !== -1
+var insane = process.argv.indexOf('--insane') !== -1
 
 if (verbose || insane) {
-    process.env.GH_VERBOSE = true;
+    process.env.GH_VERBOSE = true
 }
 
 if (insane) {
-    process.env.GH_VERBOSE_INSANE = true;
+    process.env.GH_VERBOSE_INSANE = true
 }
 
-require('../lib/cmd.js').run();
+require('../lib/cmd.js').run()
