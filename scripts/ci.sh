ecc23c86021221c2b382f7fff5a664abfbbcbfeb
Ryan Garant <ryantgarant@gmail.com>
2018-10-03 09:43:34 -0700

    chore(license): add license to script bash files

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# © 2013 Liferay, Inc. <https://liferay.com> and Node GH contributors
+# (see file: CONTRIBUTORS)
+# SPDX-License-Identifier: BSD-3-Clause
 
 if [ "${TRAVIS_REPO_SLUG}" != "node-gh/gh" ] ||
     [ "${TRAVIS_PULL_REQUEST}" != "false" ] ||
