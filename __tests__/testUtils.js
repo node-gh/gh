cda9e3ba2baa4646f0ea35fa766b7190add2df59
Ryan Garant <ryantgarant@gmail.com>
2018-12-21 15:39:47 -0800

    test(issue): add issue new comment test

---
+++ __tests__/testUtils.js
@@ -0,0 +1,97 @@
+const { execSync } = require('child_process')
+
+exports.runCmd = function runCmd(cmd) {
+    try {
+        var result = execSync(cmd, { cwd: process.cwd() })
+    } catch (error) {
+        throw new Error(error.output.toString())
+    }
+
+    return result.toString()
+}
+
+exports.prepareTestFixtures = function prepareTestFixtures(cmdName, argv) {
+    const nockBack = require('nock').back
+    const { isArray, isPlainObject, mapValues } = require('lodash')
+    let id = 0
+    let newCmdName = null
+
+    nockBack.setMode('record')
+
+    if (cmdName === 'Issue') {
+        if (argv.length === 1) {
+            newCmdName = 'Issue'
+        } else if (argv.includes('--comment')) {
+            newCmdName = 'IssueComment'
+        }
+    }
+
+    if (!newCmdName) {
+        throw new Error(`Cmd flags do not match any of our test fixtures for ---> ${cmdName}`)
+    }
+
+    nockBack.fixtures = `${process.cwd()}/__tests__/nockFixtures`
+
+    return nockBack(`${newCmdName}.json`, {
+        before,
+        afterRecord,
+    })
+
+    /* --- Normalization Functions --- */
+
+    function normalize(value, key) {
+        if (isPlainObject(value)) {
+            return mapValues(value, normalize)
+        }
+
+        if (key.includes('_at')) {
+            return '2017-10-10T16:00:00Z'
+        }
+
+        if (key.includes('_count')) {
+            return 42
+        }
+
+        if (key.includes('id')) {
+            return 1000 + id++
+        }
+
+        if (key.includes('node_id')) {
+            return 'MDA6RW50aXR5MQ=='
+        }
+
+        if (key.includes('url')) {
+            return value.replace(/[1-9][0-9]{2,10}/, '000000001')
+        }
+
+        return value
+    }
+
+    function afterRecord(fixtures) {
+        const normalizedFixtures = fixtures.map(fixture => {
+            delete fixture.rawHeaders
+
+            fixture.path = stripAccessToken(fixture.path)
+
+            if (isArray(fixture.response)) {
+                fixture.response = fixture.response.slice(0, 3).map(res => {
+                    return mapValues(res, normalize)
+                })
+            } else {
+                fixture.response = mapValues(fixture.response, normalize)
+            }
+
+            return fixture
+        })
+
+        return normalizedFixtures
+    }
+
+    function stripAccessToken(path) {
+        return path.replace(/access_token(.*?)(&|$)/, '')
+    }
+
+    function before(scope) {
+        scope.filteringPath = () => stripAccessToken(scope.path)
+    }
+}
--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
     coveragePathIgnorePatterns: ['node_modules'],
     coverageThreshold: {
         global: {
-            statements: 3.3,
-            branches: 0.5,
-            functions: 0.7,
-            lines: 3.3,
+            statements: 3,
+            branches: 0.3,
+            functions: 0.5,
+            lines: 3,
         },
     },
     testMatch: ['**/?(*.)+(spec|test).js'],
