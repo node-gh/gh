68ec4ae72928734e03220a9febac9ce523361c0a
Ryan Garant <ryantgarant@gmail.com>
2019-02-05 11:17:48 -0800

    test(pull-request): fix breaking tests due to hidden ansi characters

--- __tests__/testUtils.ts
+++ __tests__/testUtils.ts
@@ -2,10 +2,10 @@ import { execSync } from 'child_process'
 
 export function runCmd(cmd) {
     try {
-        var result = execSync(cmd, { cwd: process.cwd() })
+        var result = execSync(cmd, { cwd: process.cwd(), encoding: 'utf8' })
     } catch (error) {
         throw new Error(error.output.toString())
     }
 
-    return result.toString()
+    return result
 }
