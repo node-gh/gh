30e8a1650ecc419d6b596118964f457c066dd5c6
Ryan Garant <ryantgarant@gmail.com>
2019-02-05 18:17:04 -0800

    chore(license): add license to utils files

--- __tests__/testUtils.ts
+++ __tests__/testUtils.ts
@@ -1,3 +1,9 @@
+/**
+ * © 2013 Liferay, Inc. <https://liferay.com> and Node GH contributors
+ * (see file: CONTRIBUTORS)
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 import { execSync } from 'child_process'
 
 export function runCmd(cmd) {
