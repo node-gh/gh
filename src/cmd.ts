0575331d7fb3da9d0a629aa387d202b45161c604
Ryan Garant <ryantgarant@gmail.com>
2019-11-03 15:01:49 -0800

    feat(cmd): create function that will open a temp file in editor to store a msg
    This will lay the basis for any case where a user would prefer to use their default editor
    
    re #34

--- src/cmd.ts
+++ src/cmd.ts
@@ -21,6 +21,9 @@ import { createGlobalConfig, getGlobalPackageJson, getUserHomePath } from './con
 import { prepend, safeReaddir, safeImport, safeRealpath, safeWhich } from './fp'
 import * as git from './git'
 import { getGitHubInstance } from './github'
+import { spawnSync, execSyncInteractiveStream } from './exec'
+import { readFileSync, writeFileSync } from 'fs'
+import * as userhome from 'userhome'
 
 const testing = process.env.NODE_ENV === 'testing'
 
@@ -211,6 +214,26 @@ export async function buildOptions(args, cmdName) {
     return options
 }
 
+function openFileInEditor(fileName: string, msg: string) {
+    const filePath = userhome(fileName)
+
+    writeFileSync(filePath, msg)
+
+    const editor = spawnSync('git', ['config', '--global', 'core.editor']).stdout
+
+    execSyncInteractiveStream(`${editor} "${filePath}"`)
+
+    const newFileContents = readFileSync(filePath).toString()
+
+    const parsedFileContents = newFileContents
+        .split('\n')
+        .filter(line => !line.startsWith('#'))
+        .join('\n')
+        .trim()
+
+    console.log(parsedFileContents)
+}
+
 /* !! IMPURE CALLING CODE !! */
 export async function run() {
     process.env.GH_PATH = path.join(__dirname, '../')
@@ -220,6 +243,10 @@ export async function run() {
     }
 
     notifyVersion()
+
+    openFileInEditor('temp-gh-issue-title.txt', '# Add a pr title msg on the next line')
+
+    return
 
     getCommand(process.argv).fork(
         errMsg => console.log(errMsg),
