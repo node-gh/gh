38768b1ea417f7f786dc09c70890483183dc74e8
Ryan Garant <ryantgarant@gmail.com>
2019-11-06 13:52:24 -0800

    refactor(pull-request): move pr fetch handler to fetch file

--- src/cmds/pull-request/fetch.ts
+++ src/cmds/pull-request/fetch.ts
@@ -5,6 +5,51 @@
  */
 import * as git from '../../git'
 import { getPullRequest, FETCH_TYPE_SILENT } from './index'
+import { afterHooks, beforeHooks } from '../../hooks'
+import * as logger from '../../logger'
+
+const FETCH_TYPE_CHECKOUT = 'checkout'
+const FETCH_TYPE_MERGE = 'merge'
+const FETCH_TYPE_REBASE = 'rebase'
+
+export async function fetchHandler(options) {
+    let fetchType = FETCH_TYPE_CHECKOUT
+
+    if (options.merge) {
+        fetchType = FETCH_TYPE_MERGE
+    } else if (options.rebase) {
+        fetchType = FETCH_TYPE_REBASE
+    }
+
+    await beforeHooks('pull-request.fetch', { options })
+
+    let operation = ''
+    let branch = options.pullBranch
+
+    if (options.merge) {
+        operation = ' and merging'
+        branch = options.currentBranch
+    }
+
+    if (options.rebase) {
+        operation = ' and rebasing'
+        branch = options.currentBranch
+    }
+
+    logger.log(
+        `Fetching pull request ${logger.colors.green(
+            `#${options.number}`
+        )}${operation} into branch ${logger.colors.green(branch)}`
+    )
+
+    try {
+        await fetch(options, fetchType)
+    } catch (err) {
+        throw new Error(`Can't fetch pull request ${options.number}.\n${err}`)
+    }
+
+    await afterHooks('pull-request.fetch', { options })
+}
 
 export async function fetch(options, optType) {
     try {
