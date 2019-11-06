92b3f9976ed81f498d3e9d33e23e08970f0e3c2a
Ryan Garant <ryantgarant@gmail.com>
2019-11-06 13:56:21 -0800

    refactor(pull-request): move pr open handler to open file

--- src/cmds/pull-request/open.ts
+++ src/cmds/pull-request/open.ts
@@ -4,6 +4,24 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 import { getPullRequest, updatePullRequest, STATE_OPEN } from './index'
+import { afterHooks, beforeHooks } from '../../hooks'
+import * as logger from '../../logger'
+
+export async function openHandler(options) {
+    await beforeHooks('pull-request.open', { options })
+
+    logger.log(`Opening pull request ${logger.colors.green(`#${options.number}`)}`)
+
+    try {
+        var { data } = await open(options)
+    } catch (err) {
+        logger.error(`Can't open pull request ${options.number}.`)
+    }
+
+    logger.log(data.html_url)
+
+    await afterHooks('pull-request.open', { options })
+}
 
 export async function open(options) {
     const { data: pull } = await getPullRequest(options)
