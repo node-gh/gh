edf84407b316c4781b43d0832242edb54daf5045
Ryan Garant <ryantgarant@gmail.com>
2019-11-06 13:53:57 -0800

    refactor(pull-request): move pr close handler to close file

--- src/cmds/pull-request/close.ts
+++ src/cmds/pull-request/close.ts
@@ -4,9 +4,29 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 import * as git from '../../git'
-import { getPullRequest, updatePullRequest } from './index'
+import { getPullRequest, updatePullRequest, setMergeCommentRequiredOptions } from './index'
+import { afterHooks, beforeHooks } from '../../hooks'
+import * as logger from '../../logger'
 
 const STATE_CLOSED = 'closed'
+
+export async function closeHandler(options) {
+    await beforeHooks('pull-request.close', { options })
+
+    logger.log(`Closing pull request ${logger.colors.green(`#${options.number}`)}`)
+
+    try {
+        var { data } = await close(options)
+    } catch (err) {
+        throw new Error(`Can't close pull request ${options.number}.\n${err}`)
+    }
+
+    logger.log(data.html_url)
+
+    options = setMergeCommentRequiredOptions(options)
+
+    await afterHooks('pull-request.close', { options })
+}
 
 export async function close(options) {
     const { data: pull } = await getPullRequest(options)
